1215f7d7ae27c4334324ce7fea0a14324dea4c62
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2026-01-11 07:34:17 -0300

    feat: remove FormController route for form submissions

--- app-modules/cms/routes/cms-routes.php
+++ app-modules/cms/routes/cms-routes.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use ClintonRocha\CMS\Http\Controllers\FormController;
 use ClintonRocha\CMS\Http\Controllers\PagePreviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{slug}', [PagePreviewController::class, 'show']);
-Route::post('/forms/{formId}', [FormController::class, 'submit']);
